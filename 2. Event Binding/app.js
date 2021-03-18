fd084bb2f145005a3df1a3ba59856746804ee598
Kushal Suthar <kushalsuthar555@gmail.com>
2021-03-18 21:48:09 +0530

    Day 2

--- 2. Event Binding/app.js
+++ 2. Event Binding/app.js
@@ -2,12 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ""
+      name: "",
+      lastName: "",
+      // fullname: ''
     };
   },
   methods: {
-    setName(event,lastName){
-      this.name = event.target.value + " "+ lastName;
+    outputFullName(){
+      console.log(" Getting outputFullName... ");
+      return this.name + ' '+ 'Suthar';
+    },
+    setName(event){
+      this.name = event.target.value;
     },
     addCounter(num){
       this.counter = this.counter + num;
@@ -17,8 +23,26 @@ const app = Vue.createApp({
     },
     submitForm(){
       alert("form is submitted");
+    },
+    resetInput(){
+      this.name = '';
     }
   },
+  computed: {
+    fullName(){
+      if(this.name)
+      return this.name+ ' ' + this.lastName;
+      else
+      return '';
+    }
+  },
+  watch: {
+    counter(value){
+      setTimeout(()=>{
+        this.counter = 0;
+      },5000)
+    }
+  }
 });
 
 app.mount('#events');
---
+++ 4. Component Introduction/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            detailsAreVisible: true,
+            friends:[
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible= !this.detailsAreVisible;
+        }
+    },
+});
+
+app.component('friend-contact',{
+    template: `
+        <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">{{ detailsAreVisible? 'Hide': 'Show' }} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>`,
+    data() {
+        return {
+            detailsAreVisible: true,
+            friend:{
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com'
+            },
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible= !this.detailsAreVisible;
+        }
+    },
+});
+
+app.mount("#app");
